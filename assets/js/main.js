8abb6325ba85d78e3f7fc9308eea32b0b294a5d2
munuhee <murichustephen@gmail.com>
2023-10-16 21:25:25 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -328,14 +328,77 @@ window.addEventListener("scroll", function () {
   }
 });
 
-let items = document.querySelectorAll(".itemm");
 
-items.forEach((itemm) => {
-  itemm.addEventListener("mouseover", () => {
-    document.querySelector("#lemon").style.animationPlayState = "paused";
-  });
+/*----------------------------------------
+---------slider logic--------------------
+--------------------------------------*/
+$(window).on('load', function(){
+  // Please run it with window.onload, not with document.ready
+  initSmoothScrolling('.block','smoothscroll');
+});
 
-  itemm.addEventListener("mouseout", () => {
-    document.querySelector("#lemon").style.animationPlayState = "running";
-  });
+function initSmoothScrolling(container,animation){
+ /*
+	* @param {String} container Class or ID of the animation container
+	* @param {String} animation Name of the animation, e.g. smoothscroll
+	*/
+	var sliderWidth = 0;	
+	var animationWidth = 0;	
+	var sliderHeight = $('>div>div:first-of-type',container).outerHeight(false);
+
+	$('>div>div', container).each(function(){				
+		animationWidth += $(this).outerWidth(false);		
+	});
+	
+	// detect number of visible slides
+	var slidesVisible = $(container).width() / $('>div>div:first-of-type',container).outerWidth(false);	
+	slidesVisible = Math.ceil(slidesVisible);
+
+  // count slides to determine animation speed
+	var slidesNumber = $('>div>div', container).length;
+	var speed = slidesNumber*2;
+	
+// append the tail	
+	$('>div>div',container).slice(0,slidesVisible).clone().appendTo($('>div',container));	
+
+	// Detect the slider width with appended tail
+	$('>div>div', container).each(function(){
+		sliderWidth += $(this).outerWidth(false);
+	});
+
+	// set slider dimensions
+	$('>div',container).css({'width':sliderWidth,'height':sliderHeight});
+  
+// Insert styles to html
+	$("<style type='text/css'>@keyframes "+animation+" { 0% { margin-left: 0px; } 100% { margin-left: -"+animationWidth+"px; } } "+$('>div>div:first-of-type',container).selector+" { -webkit-animation: "+animation+" "+speed+"s linear infinite; -moz-animation: "+animation+" "+speed+"s linear infinite; -ms-animation: "+animation+" "+speed+"s linear infinite; -o-animation: "+animation+" "+speed+"s linear infinite; animation: "+animation+" "+speed+"s linear infinite; }</style>").appendTo("head");	
+
+	// restart the animation (e.g. for safari & ie)	
+	var cl = $(container).attr("class");
+	$(container).removeClass(cl).animate({'nothing':null}, 1, function () {
+		$(this).addClass(cl);
+	});
+}
+
+/*---------------------------------
+back to top------------------------
+-----------------------------*/
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var backButton = document.getElementById("back-to-top-button");
+  var chatButton = document.querySelector('.whatsapp-chat-button');
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backButton.style.display = "block";
+    chatButton.style.display = "block";
+  } else {
+    backButton.style.display = "none";
+    chatButton.style.display = "none";
+  }
+}
+
+document.getElementById("back-to-top-button").addEventListener("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
